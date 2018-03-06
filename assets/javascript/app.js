8eadd45ada310aaef451ff497369ff9e4327db42
Elizabeth Lark <elizabethlark@Elizabeths-MacBook-Pro.local>
2018-03-05 21:57:34 -0500

    updated code to show the final results after the clock runs down to zero

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,9 @@ var time = 15;
 var correctAnswer = 0;  
 var incorrectAnswer = 0; 
 var unanswered = 0;
-var final;
 
 var timer = setInterval (decrement, 1000);
+$(".center-finish").hide();
 
 function decrement () {
         $("#time-left").html(time);
@@ -13,7 +13,9 @@ function decrement () {
             if (time === 0) {
                 clearInterval(timer);
                 checkAnswers ();
-                final;
+                $(".center-finish").show();
+                $("form").hide();
+                
             }
     }
 function checkAnswers () {
@@ -34,7 +36,6 @@ function checkAnswers () {
                 
             }
         } 
-         
 console.log("Correct: " + correctAnswer);
 console.log("Wrong: " + incorrectAnswer);
 console.log("No Answer: " + unanswered);
@@ -45,7 +46,3 @@ $("#unanswered").append(unanswered);
 
 
 
-// figure out how to get the final results for answers to show up in finish page results
-//finish page is reading the globally declared variables; maybe need to push to an array
-// or convert to a string somehow so that results will show on page? 
-//Figure out how to make github start on the start page (vs the index.html page)
